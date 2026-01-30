d3e8818866427d40e66b37d24aed80343088850a
ouadie <ouadieffantouz@gmail.com>
2026-01-30 22:30:02 +0100

    first change

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt officia illum tempore ullam magni quidem enim, quasi soluta similique velit expedita? Perspiciatis adipisci alias id tempore cum dolor consectetur! Cumque.</h1>
+</body>
+</html>
